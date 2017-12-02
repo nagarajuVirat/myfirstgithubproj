db4a790f6d8e2c5459c47d7cc60db3882cffdd48
nagarajuVirat <nagarajuchnpalli@gmail.com>
2017-12-03 00:03:52 +0530

    initial commit

---
+++ src/main/java/com/github/lab/myfirstgithubproj/MyfirstgithubprojApplication.java
@@ -0,0 +1,12 @@
+package com.github.lab.myfirstgithubproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstgithubprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstgithubprojApplication.class, args);
+	}
+}
---
+++ src/test/java/com/github/lab/myfirstgithubproj/MyfirstgithubprojApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.lab.myfirstgithubproj;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyfirstgithubprojApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
